665142bcd4a9c90460f2257f87b970df556764b6
venkat-php <venkatesh.cse.tec@gmail.com>
2021-01-06 13:21:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 
 function App() {
   return (
@@ -7,8 +12,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+           Welcome To JavaTpoint. 
+           
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Hello this is Venkateswarlu Dande.<br></br>This is my first React App</h1>
+        <h1>Hello this is Dande Venkateswarlu.<br></br>This is my first React App</h1>
+        <h1>Hello this is Dande Venkateswarlu.<br></br>This is my first React App</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +32,7 @@ function App() {
   );
 }
 
-export default App;
+
+
+//export default App;
+export default withAuthenticator(App);
